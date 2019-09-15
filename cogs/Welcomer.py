abadae78f2c06f2f36f5e0ad8274f26f9bcce044
Alex Hart <alex.hart@scientificgames.com>
2019-09-15 15:52:45 -0400

    broken attempt at rules reaction listener

--- cogs/Welcomer.py
+++ cogs/Welcomer.py
@@ -1,9 +1,10 @@
-from discord import Message
+from discord import Message, Reaction, TextChannel
 from discord.ext import commands
 from discord.ext.commands import Context, command
 
 from cogs.BaseCog import BaseCog
-from utils import Configuration, Logging
+from sky import Skybot
+from utils import Configuration, Logging, Emoji
 
 
 class Welcomer(BaseCog):
@@ -21,7 +22,6 @@ class Welcomer(BaseCog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        channel = member.guild.system_channel
         txt = Configuration.get_var("welcome_msg")
         rules_channel = self.bot.get_channel(Configuration.get_var('rules_channel'))
         welcome_channel = self.bot.get_channel(Configuration.get_var('welcome_channel'))
@@ -29,6 +29,24 @@ class Welcomer(BaseCog):
         Logging.info(txt)
         if welcome_channel is not None:
             await welcome_channel.send(txt)
+
+
+async def init_rules_reaction(bot):
+    rules_channel_id = Configuration.get_var('rules_channel')
+    rules_message_id = Configuration.get_var('rules_react_message_id')
+    new_member_role = Configuration.get_var('new_member_role')
+    emoji = Emoji.get_emoji("CANDLE")
+    rules_channel: TextChannel = bot.get_channel(rules_channel_id)
+    rules_message: Message = await rules_channel.fetch_message(rules_message_id)
+
+    def check(reaction: Reaction, user):
+        return user != bot.user and str(reaction.emoji) == emoji and reaction.message.id == rules_message_id
+
+    while True:
+        reaction, user = await bot.wait_for('reaction_add', check=check)
+        if user:
+            user.add_roles(new_member_role)
+            Logging.info(f"{user.mention} added a reaction!")
 
 
 def setup(bot):
