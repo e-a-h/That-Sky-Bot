a67836aa2327fd4a1272e42c17524a1f52db0c19
AEnterprise <aenterprise2@gmail.com>
2019-09-16 16:05:45 +0200

    hook up reaction handlers, make multiple roles possible from the config

--- cogs/Bugs.py
+++ cogs/Bugs.py
@@ -1,6 +1,8 @@
 import asyncio
+from concurrent.futures import CancelledError
 
 from discord import Forbidden, Embed, NotFound
+from discord.ext import commands
 from discord.ext.commands import Context, command
 
 from cogs.BaseCog import BaseCog
@@ -13,6 +15,9 @@ class Bugs(BaseCog):
     def __init__(self, bot):
         super().__init__(bot)
         bot.loop.create_task(self.startup_cleanup())
+        self.bug_messages = set()
+        self.in_progress = dict()
+        self.canceling = set()
 
     async def shutdown(self):
         for name, cid in Configuration.get_var("channels").items():
@@ -41,106 +46,130 @@ class Bugs(BaseCog):
                 pass
             else:
                 await message.delete()
+                if message.id in self.bug_messages:
+                    self.bug_messages.remove(message.id)
         message = await channel.send(Lang.get_string("bug_info"))
         bugemoji = Emoji.get_emoji('BUG')
         await message.add_reaction(bugemoji)
-        # TODO: wait for reaction. trigger bug report
-        # user = await self.bot.wait_for('reaction_add')
+        self.bug_messages.add(message.id)
         Configuration.set_persistent_var(f"{key}_message", message.id)
 
     @command()
     async def bug(self, ctx: Context):
+        # remove command to not flood chat (unless we are in a DM already)
+        if ctx.guild is not None:
+            await ctx.message.delete()
         await self.report_bug(ctx.author, ctx.channel)
 
     async def report_bug(self, user, trigger_channel):
-        # remove command to not flood chat (unless we are in a DM already)
-        # if ctx.guild is not None:
-        #     await ctx.message.delete()
+        if user.id in self.in_progress:
+            if user.id in self.canceling:
+                await trigger_channel.send(f"{user.mention} stop spamming the bug reaction!", delete_after=10)
+                return
 
-        channel = await user.create_dm()
+            async def start_over():
+                stop_cancel()
+                self.in_progress[user.id].cancel()
+                del self.in_progress[user.id]
+                await self.report_bug(user, trigger_channel)
 
-        # vars to store everything
-        asking = True
-        platform = ""
-        branch = ""
-        additional = False
-        additional_text = ""
-        attachments = False
-        attachment_links = []
-        attachment_message = ""
-        report = None
-        member_mention = ""
+            def stop_cancel():
+                self.canceling.remove(user.id)
 
-        # define all the parts we need as inner functions for easier sinfulness
+            self.canceling.add(user.id)
+            await Questions.ask(self.bot, trigger_channel, user, f"{user.mention} You are already in the middle of reporting a bug, do you want to cancel that report and start over?",
+                                [
+                                    Questions.Option("YES", handler=start_over),
+                                    Questions.Option("NO", handler=stop_cancel)
+                                ], delete_after=True)
 
-        async def abort():
-            nonlocal asking
-            await user.send(
-                "No? Alright then, the devs won't be able to look into it but feel free to return later to report it then!")
-            asking = False
+            return
+        task = self.bot.loop.create_task(self.actual_bug_reporter(user, trigger_channel))
+        self.in_progress[user.id] = task
 
-        def set_platform(p):
-            nonlocal platform
-            platform = p
 
-        def set_branch(b):
-            nonlocal branch
-            branch = b
+    async def actual_bug_reporter(self, user, trigger_channel):
+        # wrap everything so users can't get stuck in limbo
+        try:
+            channel = await user.create_dm()
 
-        def add_additional():
-            nonlocal additional
-            additional = True
+            # vars to store everything
+            asking = True
+            platform = ""
+            branch = ""
+            additional = False
+            additional_text = ""
+            attachments = False
+            attachment_links = []
+            attachment_message = ""
+            report = None
 
-        def add_attachments():
-            nonlocal attachments
-            attachments = True
+            # define all the parts we need as inner functions for easier sinfulness
 
-        def verify_version(v):
-            if "latest" in v:
-                return "Please specify a version number, ``latest`` is not valid because what is latest changes overtime. You can also think you have latest but a newer update might be available if you didn't check recently"
-            # TODO: double check if we actually want to enforce this
-            if len(Utils.NUMBER_MATCHER.findall(v)) is 0:
-                return "There don't appear to be any numbers in there"
-            if len(v) > 20:
-                return "Whoa there, i just need a version number, not an entire love letter."
-            return True
+            async def abort():
+                nonlocal asking
+                await user.send(
+                    "No? Alright then, the devs won't be able to look into it but feel free to return later to report it then!")
+                asking = False
 
-        def max_length(length):
-            def real_check(text):
-                if len(text) > length:
-                    return "That text seems suspiciously long for the question asked, please shorten it a bit"
+            def set_platform(p):
+                nonlocal platform
+                platform = p
+
+            def set_branch(b):
+                nonlocal branch
+                branch = b
+
+            def add_additional():
+                nonlocal additional
+                additional = True
+
+            def add_attachments():
+                nonlocal attachments
+                attachments = True
+
+            def verify_version(v):
+                if "latest" in v:
+                    return "Please specify a version number, ``latest`` is not valid because what is latest changes overtime. You can also think you have latest but a newer update might be available if you didn't check recently"
+                # TODO: double check if we actually want to enforce this
+                if len(Utils.NUMBER_MATCHER.findall(v)) is 0:
+                    return "There don't appear to be any numbers in there"
+                if len(v) > 20:
+                    return "Whoa there, i just need a version number, not an entire love letter."
                 return True
 
-            return real_check
+            def max_length(length):
+                def real_check(text):
+                    if len(text) > length:
+                        return "That text seems suspiciously long for the question asked, please shorten it a bit"
+                    return True
 
-        async def send_report():
-            # save report in the database
-            # TODO: send attachment message id to db?
-            br = BugReport.create(reporter=user.id, platform=platform, platform_version=platform_version, branch=branch,
-                                  app_version=app_version, title=title, steps=steps, expected=expected,
-                                  additional=additional)
-            for a in attachment_links:
-                Attachements.create(report=br, url=a)
+                return real_check
 
-            # send report
-            channel_name = f"{platform}_{branch}".lower()
-            c = Configuration.get_var("channels")[channel_name]
-            message = await self.bot.get_channel(c).send(content=member_mention, embed=report)
-            if attachment_message:
-                attachment = await self.bot.get_channel(c).send(attachment_message)
-                br.attachment_message_id = attachment.id
-            br.message_id = message.id
-            br.save()
-            await message.edit(content=member_mention.replace("##", f'#{br.id}'))
-            if attachment_message:
-                await attachment.edit(content=attachment_message.replace("##", f'#{br.id}'))
-            await channel.send(f"Thank you! Your report was successfully sent and can be found in <#{c}>!")
-            await self.send_bug_info(channel_name)
+            async def send_report():
+                # save report in the database
+                br = BugReport.create(reporter=user.id, platform=platform, platform_version=platform_version,
+                                      branch=branch, app_version=app_version, title=title, steps=steps,
+                                      expected=expected, additional=additional_text)
+                for a in attachment_links:
+                    Attachements.create(report=br, url=a)
 
-        async def restart():
-            await self.report_bug(user, trigger_channel)
+                # send report
+                channel_name = f"{platform}_{branch}".lower()
+                c = Configuration.get_var("channels")[channel_name]
+                message = await self.bot.get_channel(c).send(
+                    content=f"**Bug Report {br.id} - submitted by {user.mention}**", embed=report)
+                if attachment_message:
+                    attachment = await self.bot.get_channel(c).send(f"**Attachment to report {br.id}**\n{attachment_message}")
+                    br.attachment_message_id = attachment.id
+                br.message_id = message.id
+                br.save()
+                await channel.send(f"Thank you! Your report was successfully sent and can be found in <#{c}>!")
+                await self.send_bug_info(channel_name)
 
-        try:
+            async def restart():
+                self.in_progress.remove(user.id)
+                await self.report_bug(user, trigger_channel)
 
             await Questions.ask(self.bot, channel, user,
                                 """```css
@@ -192,13 +221,13 @@ Are you using the Live game or a Beta version? The beta version is installed thr
                 # question 4: sky app version
                 app_version = await Questions.ask_text(self.bot, channel, user, """```css
 Sky App Version Number```
-What version of the sky app where you using when you experienced the bug?
+What **version** of the sky app where you using when you experienced the bug?
 # TODO: instructions
 """, validator=verify_version)
 
                 app_build = await Questions.ask_text(self.bot, channel, user, """```css
 Sky App Build Number```
-What build of the sky app where you using when you experienced the bug?
+What **build** of the sky app where you using when you experienced the bug?
 # TODO: instructions
 """, validator=verify_version)
 
@@ -211,7 +240,7 @@ Provide a brief title or topic for your bug.
 
                 steps = await Questions.ask_text(self.bot, channel, user, """```css
 How to Reproduce the Bug```
-How did the bug occur? Provide a description that will help us rebproduce the problem, if possible in easily reproducible steps. Example:
+How did the bug occur? Provide a description that will help us reproduce the problem, if possible in easily reproducible steps. Example:
 ```- step 1
 - step 2
 - step 3```
@@ -252,7 +281,6 @@ Do you have any attachments to add to this report?""",
                 # assemble the report
                 report = Embed()
                 report.set_author(name=f"{user} ({user.id})", icon_url=user.avatar_url_as(size=32))
-
                 report.add_field(name="Platform", value=f"{platform} {platform_version}")
                 report.add_field(name="Sky app version", value=app_version)
                 report.add_field(name="Sky app build", value=app_build)
@@ -263,22 +291,17 @@ Do you have any attachments to add to this report?""",
                     report.add_field(name="Additional information", value=additional_text, inline=False)
                 # if attachments:
                 #     report.add_field(name="Attachment(s)", value="\n".join(attachment_links))
-
-                member_mention = f"**Bug Report ## - submitted by {user.mention}**"
-                await channel.send(content=member_mention, embed=report)
-
+                await channel.send(content=f"**Bug Report ## - submitted by {user.mention}**", embed=report)
                 if attachment_links:
-                    attachment_message = '**Attachment to report ##**\n'
+                    attachment_message = ''
                     for a in attachment_links:
                         attachment_message += f"{a}\n"
                     await channel.send(attachment_message)
-
                 await Questions.ask(self.bot, channel, user, "Does the above report look alright?",
                                     [
                                         Questions.Option("YES", "Yes, send this report", send_report),
                                         Questions.Option("NO", "Nope, i made a mistake. Start over", restart)
                                     ])
-
             else:
                 return
 
@@ -286,8 +309,23 @@ Do you have any attachments to add to this report?""",
             await trigger_channel.send(
                 "I was unable to DM you for questions about your bug, could you please (temp) allow DMs from this server and try again? You can enable this in the privacy settings, found in the server dropdown menu",
                 delete_after=30)
-        except asyncio.TimeoutError:
-            pass
+        except (asyncio.TimeoutError, CancelledError):
+            del self.in_progress[user.id]
+        except Exception as ex:
+            del self.in_progress[user.id]
+            await Utils.handle_exception("bug reporting", self.bot, ex)
+            raise ex
+        else:
+            del self.in_progress[user.id]
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, event):
+        if event.message_id in self.bug_messages and event.user_id != self.bot.user.id:
+            user = self.bot.get_user(event.user_id)
+            channel = self.bot.get_channel(event.channel_id)
+            message = await channel.fetch_message(event.message_id)
+            await message.remove_reaction(event.emoji, user)
+            await self.report_bug(user, channel)
 
 
 def setup(bot):
