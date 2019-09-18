7b0607f2d8f7b4377c6896ee56d0524a8987fce8
AEnterprise <aenterprise2@gmail.com>
2019-09-18 14:24:25 +0200

    copy over CustCommands from GearBot

--- utils/Database.py
+++ utils/Database.py
@@ -46,8 +46,18 @@ class Repros(Model):
         database = connection
 
 
+class CustomCommand(Model):
+    id = PrimaryKeyField()
+    serverid = BigIntegerField()
+    trigger = CharField(max_length=20, collation="utf8mb4_general_ci")
+    response = CharField(max_length=2000, collation="utf8mb4_general_ci")
+
+    class Meta:
+        database = connection
+
+
 def init():
     global connection
     connection.connect()
-    connection.create_tables([BugReport, Attachements, Repros])
+    connection.create_tables([BugReport, Attachements, Repros, CustomCommand])
     connection.close()
