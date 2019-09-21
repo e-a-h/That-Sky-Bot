45b952a4b6f728bf49e7496b9df03cfb95649edf
AEnterprise <aenterprise2@gmail.com>
2019-09-21 14:14:16 +0200

    temp add some debug logging

--- cogs/Welcomer.py
+++ cogs/Welcomer.py
@@ -16,7 +16,11 @@ class Welcomer(BaseCog):
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, event):
         react_user_id = event.user_id
+        Logging.info(react_user_id)
         rules_message_id = Configuration.get_var('rules_react_message_id')
+        Logging.info(rules_message_id)
+        Logging.info(event.message_id)
+        Logging.info(str(event.emoji))
         if react_user_id != self.bot.user.id and event.message_id == rules_message_id:
             await self.handle_reaction_change("add", str(event.emoji), react_user_id)
 
@@ -29,10 +33,13 @@ class Welcomer(BaseCog):
 
     async def handle_reaction_change(self, t, reaction, user_id):
         roles = Configuration.get_var("roles")
+        Logging.info(roles)
         if reaction in roles:
             guild = self.bot.get_guild(Configuration.get_var("guild_id"))
             role = guild.get_role(roles[reaction])
             member = guild.get_member(user_id)
+            Logging.info(role)
+            Logging.info(member)
             action = getattr(member, f"{t}_roles")
             await action(role)
 
