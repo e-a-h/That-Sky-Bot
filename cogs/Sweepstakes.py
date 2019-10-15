14c6377a1fa2dd2674b09ddffe05507b3b82898b
Alex Hart <alex.hart@scientificgames.com>
2019-10-15 10:21:37 -0400

    sweeps initial commit

---
+++ cogs/Sweepstakes.py
@@ -0,0 +1,83 @@
+import os
+
+from discord import File
+from discord.ext import commands
+
+from cogs.BaseCog import BaseCog
+from utils import Utils, Logging
+from datetime import datetime
+from utils.Utils import save_to_disk
+
+
+class Sweepstakes(BaseCog):
+
+    def __init__(self, bot):
+        super().__init__(bot)
+
+    async def cog_check(self, ctx):
+        if not hasattr(ctx.author, 'guild'):
+            return False
+        # TODO: this should probably be admin and/or custom role
+        return ctx.author.guild_permissions.manage_channels
+
+    async def send_csv(self, ctx, user_list: dict):
+        fields = ["id", "nick", "username", "discriminator", "mention"]
+        data_list = ()
+        for user_id, user in user_list.items():
+            data_list += ({"id": user.id,
+                           "nick": user.nick,
+                           "username": user.name,
+                           "discriminator": user.discriminator,
+                           "mention": user.mention},)
+        now = datetime.today().timestamp()
+        out = ""
+        for i in data_list:
+            out += str(i) + "\n"
+
+        await ctx.send(f"There are {len(data_list)} entries (unique reactions) to this drawing.")
+        save_to_disk(f"entries_{now}", data_list, 'csv', fields)
+        send_file = File(f"entries_{now}.csv")
+        await ctx.send(file=send_file)
+        os.remove(f"entries_{now}.csv")
+
+    @commands.group(name="sweeps")
+    @commands.guild_only()
+    @commands.bot_has_permissions(embed_links=True)
+    async def sweepstakes(self, ctx: commands.Context):
+        """sweeps help"""
+        if ctx.invoked_subcommand is None:
+            pass
+
+    @sweepstakes.command(aliases=["reactions", "gr", "entries"])
+    @commands.guild_only()
+    async def getreactions(self, ctx: commands.Context, jump_url: str):
+        """get a list of reactions to a given message"""
+        parts = jump_url.split('/')
+        channel_id = parts[-2]
+        message_id = parts[-1]
+        try:
+            channel = await self.bot.fetch_channel(channel_id)
+            message = await channel.fetch_message(message_id)
+            reaction_list = {}
+            for reaction in message.reactions:
+                async for user in reaction.users():
+                    reaction_list[user.id] = user
+                # users is now a list of User...
+                # winner = random.choice(users)
+                # await channel.send('{} has won the raffle.'.format(winner))
+            await self.send_csv(ctx, reaction_list)
+        except Exception as e:
+            await Utils.handle_exception(f"Failed to get entries {channel_id}/{message_id}", self, e)
+            await ctx.send(f"Failed to get entries {channel_id}/{message_id}")
+        # test
+        # ?sweeps reactions 621746950458572801/624267628080267264
+
+    @sweepstakes.command(aliases=["csv"])
+    @commands.guild_only()
+    async def report(self, ctx: commands.Context, message_id):
+        """get a list of reactions to a given message and export as CSV"""
+
+
+
+def setup(bot):
+    bot.add_cog(Sweepstakes(bot))
